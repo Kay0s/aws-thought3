a894467d6f1c8236d4b0fd37a1ac6b898ef404e7
Kay0s <hamilton.kristina@gmail.com>
2021-04-23 19:19:33 -0500

    added create-bucket.js and and created S3 user-images bucket

---
+++ server/create-bucket.js
@@ -0,0 +1,19 @@
+// Load the AWS SDK for Node.js
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+// Set the region
+AWS.config.update({region: 'us-east-2'});
+// Create S3 service object
+const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+// Create the parameters for calling createBucket
+let bucketParams = {
+    Bucket : "user-images-63cd639d-b354-4191-aa89-2d3b4c49b72f" + uuidv4()
+  };
+// call S3 to create the bucket
+s3.createBucket(bucketParams, (err, data) => {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success");
+    }
+  });
