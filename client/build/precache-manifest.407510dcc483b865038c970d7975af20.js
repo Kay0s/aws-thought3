a77251279ce7897be574ea5a269fca7463cbcb74
Kay0s <hamilton.kristina@gmail.com>
2021-04-24 14:26:33 -0500

    adding build folder

---
+++ client/build/precache-manifest.407510dcc483b865038c970d7975af20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2bad0f97ab65c505b8062aa496ec6c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00e58be4aa6ddf785892",
+    "url": "/static/css/main.4a34557b.chunk.css"
+  },
+  {
+    "revision": "32ece7a7a18511da63b6",
+    "url": "/static/js/2.b2e524db.chunk.js"
+  },
+  {
+    "revision": "0ec9300b90a1964ccb21b13048e7fb5c",
+    "url": "/static/js/2.b2e524db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00e58be4aa6ddf785892",
+    "url": "/static/js/main.7ff27823.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  }
+]);
